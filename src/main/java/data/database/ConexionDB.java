40af336c1cf3f81921807a2eb983ced36edf1a6c
tobias <tobiasceniceros@gmail.com>
2025-12-24 19:35:15 +0100

    Creacion estructura de paquetes MVVM

---
+++ src/main/java/data/database/ConexionDB.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package data.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author tobias
+ */
+public class ConexionDB {
+    public String URL = "jdbc:postgresql://localhost:5432/aprendizaje";
+    String USER = "alumno";
+    String PWD = "alumno";
+    
+    public Connection connectDataBase() throws SQLException {
+        Connection connection = DriverManager.getConnection(URL, USER, PWD);
+        return connection;
+    }
+}
